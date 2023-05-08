aaffcbe31b52bf67a4f259f878585efc5831cdec
Oleg <oleg.ustinov.1993@mail.ru>
2023-05-08 13:42:43 +0600

    second

--- JS/script.js
+++ JS/script.js
@@ -7,12 +7,30 @@ let personalMovideDB = {
     privat: false    
 }
 
+
+
+for (let i = 0; i < 2; i++) {
 let a = prompt('Один из последних просмотренных фильмов?', '');
 let b = prompt('На сколько оцените его?');
-let c = prompt('Один из последних просмотренных фильмов?', '');
-let d = prompt('На сколько оцените его?');
+if (a != '' && b != '' && a != null && b != null && a.length < 50) {
+    personalMovideDB.movies[a] = b;
+    console.log('done');
+} else {
+    console.log('error');
+    i--;
+}
 
-personalMovideDB.movies['a'] = b;
-personalMovideDB.movies['c'] = d;
+}
+if (personalMovideDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов') 
+} else if (personalMovideDB.count >= 10 && personalMovideDB.count < 30) {
+    console.log('Вы классический зритель') 
+} else if (personalMovideDB.count >= 30) {
+    console.log('Вы киноман')
+} else {
+    console.log('Произошла ошибка');
+}
 
 console.log(personalMovideDB);
+
+
